925280af5d32b905f3bf5fd6370411838e8ba7a8
Jeff Labonte <jeff.labonte12@gmail.com>
2018-12-11 23:05:45 -0500

    Draw circles with you mouse!!!

---
+++ draw_image.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+"""
+    Function
+"""
+
+
+def draw_circle(event, x, y ,flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img=img, center=(x, y), radius=10, color=(0, 255, 0), thickness=-1)
+
+
+cv2.namedWindow(winname="drawing")
+
+cv2.setMouseCallback("drawing", draw_circle)
+"""
+    Showing the image with OpenCV
+"""
+
+img = np.zeros((512, 512, 3))
+
+while True:
+    cv2.imshow("drawing", img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
