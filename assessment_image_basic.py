30cb91c0865bdd7fcbb6cf9ff9824278607f0223
Jeff Labonte <jeff.labonte12@gmail.com>
2018-12-17 19:36:33 -0500

    Add code

--- assessment_image_basic.py
+++ assessment_image_basic.py
@@ -17,3 +17,12 @@ pt1 = (200, 380)
 pt2 = (600, 700)
 cv2.rectangle(img_rgb, pt1=pt1, pt2=pt2, color=(255,0,0), thickness=10)
 plt.imshow(img_rgb)
+
+vertices = np.array([ [200, 700], [ 425, 400 ], [600, 700] ], np.int32)
+vertices.shape
+
+pts = vertices.reshape((-1, 1, 2))
+
+cv2.polylines(img_rgb, [pts], isClosed=True, color=(0,0,255), thickness=29)
+
+plt.imshow(img_rgb)
